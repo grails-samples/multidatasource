fc2a4a65f7ee6a728d6b728e861e0ba057706cd2
sdelamo <sergio.delamo@softamo.com>
2017-10-04 15:16:34 +0200

    Use @Transactional

--- grails-app/services/demo/BookService.groovy
+++ grails-app/services/demo/BookService.groovy
@@ -1,19 +1,19 @@
 package demo
 
+import grails.gorm.transactions.Transactional
+import groovy.transform.CompileStatic
+
+@Transactional
+@CompileStatic
 class BookService {
 
     Book saveBook(String name) {
-        Book.withTransaction {
-            Book book = new Book(title: name)
-            book.save()
-            book
-        }
+        Book book = new Book(title: name)
+        book.save()
+        book
     }
 
     List<Book> findAll() {
-        Book.withTransaction(readOnly: true) {
-            Book.where { }.list() as List<Book>
-        }
-
+        Book.where { }.list() as List<Book>
     }
 }
