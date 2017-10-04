7484bf5eb1691730e974f835aa39a8e07cc3d2ec
sdelamo <sergio.delamo@softamo.com>
2017-10-04 15:25:29 +0200

    Add @Transactional to method

--- grails-app/services/demo/BookService.groovy
+++ grails-app/services/demo/BookService.groovy
@@ -1,19 +1,22 @@
 package demo
 
+import grails.gorm.transactions.ReadOnly
 import grails.gorm.transactions.Transactional
 import groovy.transform.CompileStatic
 
-@Transactional
 @CompileStatic
 class BookService {
 
+    @Transactional
     Book saveBook(String name) {
         Book book = new Book(title: name)
         book.save()
         book
     }
 
+    @ReadOnly
     List<Book> findAll() {
         Book.where { }.list() as List<Book>
     }
 }
+
